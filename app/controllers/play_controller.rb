3bb6e1f5f10cff68c5cb29c8c2c0986560e1157e
Wolwer1ne <wolwerine@yandex.ru>
2023-04-18 14:26:35 +0300

    Add default photo

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -8,6 +8,7 @@ class PlayController < ApplicationController
     @link = @quest_session.quest.story['links'].find {|x| x['from'] == 1}
     @node_text = @quest_session.quest.story['nodes'].find {|x| x['id'].to_i == @quest_session.current_node}['text']
     @links = @quest_session.quest.story['links']
+    @node_image_url = "https://placekitten.com/g/640/480"
     render 'select'
   end
   def select
@@ -16,6 +17,7 @@ class PlayController < ApplicationController
     @link = @quest_session.quest.story['links'].find {|x| x['id'] == params[:link_id].to_i}
     @quest_session.current_node = @link['to']
     @node_text =  @quest_session.quest.story['nodes'].find {|x| x['id'].to_i == @quest_session.current_node}['text']
+    @node_image_url = "https://placekitten.com/g/640/480"
     @links = @quest_session.quest.story['links'].select {|x| x['from'] == @quest_session.current_node}
   end
 end
